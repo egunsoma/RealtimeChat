ff77bafa4eb3715e1b5e54ae0875205bebdea243
Dombi Soma Kristóf <dombis@sonrisa.hu>
2018-05-04 23:51:33 +0200

    Add Conversation creation and listing

--- src/scenes/ConversationListScreen.js
+++ src/scenes/ConversationListScreen.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 import {
- Text, Button, View
+ Text, Button, View, FlatList, ActivityIndicator
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import firebase from 'react-native-firebase';
 
 
 export default class ConversationListScreen extends Component {
+  state = {
+    conversations: [],
+    loading: true
+  }
+  componentWillMount() {
+    this.unsubscribe = firebase.firestore().collection('conversations')
+      .where(`members.${firebase.auth().currentUser.uid}`, '>', 0)
+      .orderBy(`members.${firebase.auth().currentUser.uid}`, 'desc') 
+      .onSnapshot(this.onCollectionUpdate.bind(this));
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  onCollectionUpdate(querySnapshot) {
+    const conversations = [];
+    querySnapshot.forEach((doc) => {
+      const { name, lastMessage } = doc.data();
+      console.log(doc);
+
+      const item = { 
+        key: doc.id,
+        name,
+        lastMessage
+      };
+
+      conversations.push(item);
+    });
+    this.setState({ 
+      conversations,
+      loading: false,
+   });
+  }
 
   onLogoutPress() {
     firebase.auth().signOut().then(() => {
@@ -14,11 +48,42 @@ export default class ConversationListScreen extends Component {
     })
   }
 
+  onCreateConversationPress() {
+    const now = new Date().getTime()
+    firebase.firestore().collection('conversations')
+      .add({
+        name: "New Conversation",
+        members: {
+          [firebase.auth().currentUser.uid]: now
+        },
+        lastMessage: { 
+          type: 'text',
+          data: 'Conversation created',
+          senderId: firebase.auth().currentUser.uid,
+          timestamp: now
+        }
+      })
+  }
+
+  renderConversation({item}) {
+    return (
+      <Text>{item.name}</Text>
+    ) 
+  }
 
   render() {
     return (
       <View>
-        <Button title="Create Conversation" onPress={() => Actions.conversation_messages()} />
+        {
+          this.state.loading ? 
+          <ActivityIndicator /> :
+          <FlatList
+            data={this.state.conversations}
+            extraData={this.state}
+            renderItem={this.renderConversation.bind(this)}
+          />
+        }
+        <Button title="Create Conversation" onPress={this.onCreateConversationPress.bind(this)} />
         <Button title="Sign out" onPress={this.onLogoutPress.bind(this)} />
       </View>
     );
